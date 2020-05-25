23000ae64acaaa88cf864914d9f9c5ca61bac4f6
ochiai-hideo <65894868+ochiai-hideo@users.noreply.github.com>
2020-05-25 18:56:09 +0900

    #1 に対する対応をしました。

--- assessment.js
+++ assessment.js
@@ -22,7 +22,7 @@ assessmentButton.onclick = () => {
     return;
   }
 
-  // 診断結果表示エリアの作成
+  // 診断結果表示エリアの作成 
   removeAllChildren(resultDivided);
   const header = document.createElement('h3');
   header.innerText = '診断結果';
